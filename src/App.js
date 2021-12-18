9d050b38a68581cc2657a7a8e324e6b5749bc4ef
Serob Kirakosyan <serob.kirakosyan.88@gmail.com>
2021-12-18 19:19:32 +0400

    Tnayin.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {useEffect, useState} from "react";
+
 
 function App() {
+  const [data, setData] = useState([]);
+  const [input, setInput] = useState("");
+  const [output, setOutput] = useState([]);
+
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/todos")
+        .then((res) => res.json())
+        .then((res) => setData(res));
+  }, []);
+  useEffect(() => {
+  setOutput([])
+      data.filter(val => {
+          if(input === ""){
+              setOutput([])
+          } else if (val.title.toLowerCase().includes(input.toLowerCase())) {
+              setOutput(output => [...output,val])
+          }
+      })
+  }, [input]);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App">
+        <div className="search-bar">
+          <input onChange={(e) => setInput(e.target.value)}
+                 type="text"
+                 placeholder="Search..."
+          />
+        </div>
+
+       <div className="output">
+          {output.map(({title, id}) =>(
+              <p key={id}>{title}</p>
+           ))}
+        </div>
     </div>
   );
 }
